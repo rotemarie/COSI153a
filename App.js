9b22e66c3beecc6696f28d04b52317f3417c93fc
rotemarie <70762029+rotemarie@users.noreply.github.com>
2022-06-21 18:33:05 +0300

    App

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+import AssetExample from './components/CPA2';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{fontWeight: 'bold', textAlign: 'center', fontFamily:'Bangla MN', fontSize: 40}}>  CHEM3D </Text>
+      <AssetExample/>
     </View>
   );
 }
@@ -13,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'lightsteelblue'
   },
+
 });
